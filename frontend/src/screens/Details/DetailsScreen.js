86ddf1b59a341c4a7b0d944301ea5abf9e02df49
AdrianJoseph99 <57686487+AdrianJoseph99@users.noreply.github.com>
2023-03-30 18:36:17 +0000

    Basic Details Page Completed

--- frontend/src/screens/Details/DetailsScreen.js
+++ frontend/src/screens/Details/DetailsScreen.js
@@ -3,9 +3,129 @@ import "./DetailsScreen.css";
 import CustomButton from "components/ui/CustomButton";
 import PageHeader from "components/sections/PageHeader";
 
+import pfp from "../../assets/images/profile_pic.png";
+import spotifyLogo from "../../assets/images/spotify_logo.png";
+
+import playlistCover1 from "../../assets/images/playlist-cover1.png";
+import playlistCover2 from "../../assets/images/playlist-cover2.png";
+import playlistCover3 from "../../assets/images/playlist-cover3.png";
+
+import Real from "../../assets/images/Album-cover-Real.jpeg";
+import Toast from "../../assets/images/Album-cover-Toast.jpeg";
+import Drake from "../../assets/images/Album-cover-Drake.jpeg";
+import September from "../../assets/images/Song-cover-September.jpg";
+import NoIdea from "../../assets/images/Album-cover-NoIdea.jpeg";
+import Vibe from "../../assets/images/Album-cover-Vibe.jpeg";
+
+import bestOfEWF from "../../assets/images/Album-cover-September.jpeg";
+
 export default function DetailsScreen() {
-  return (
-    <div></div>
-  )
+    
+    const genres = [
+        "Pop",
+        "Rock",
+        "Hip Hop",
+        "Latin",
+        "Dance",
+        "R&B",
+        "Country",
+        "Metal",
+        "Jazz",
+        "Classical",
+        "Extra one Idk"
+    ];
+
+    return (
+        <main className="user-profile">
+            {/* <PageHeader /> */}
+            <div className="user-header">
+                <div className="song-img">
+                    <img src={September} 
+                        width="250" 
+                        height="250"
+                    />
+                </div>
+                <div className="info">
+                    <div className="username">September</div>
+                    <div className="follow-info">
+                        <div className="followers">1:13 - track length</div>
+
+                    </div>
+                    <div className="interactables">
+                        <CustomButton
+                            text="★"
+                        /> 
+                        <CustomButton
+                            text="Add To Playlist"
+                            style={{ flexGrow: 0.35 }}
+                        />
+                    </div>
+                    <div className="spotifyconnect">
+                        <CustomButton
+                            text="Open In Spotify"
+                            style={{alignitems:"center",
+                                    width:400,
+                                    height:65,}}
+                            type = "PRIMARY"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="main-content"
+                class="row">
+                    <h3>Appears On:</h3>
+                    <div class="column">
+                        <Album
+                            name="The Best of Earth, Wind & Fire, Vol. 1"
+                            img={bestOfEWF}
+                        />
+                        {/* <div>See more on Spotify...</div> */}
+                    </div>
+                    <h3>Artists</h3>
+                    <div className="history-container"
+                        class="column">
+                        <SongCard
+                            artists={["Earth, Wind & Fire"]}
+                            img={September}
+                            style={{flex:5}}
+                        />
+                    </div>
+            </div>
+        </main>
+    );
 }
 
+function Album({ name, img }) {
+    return (
+        <div className="playlist-container">
+            <div className="img-container">
+                <img src={img} />
+            </div>
+            <p>{name}</p>
+        </div>
+    );
+}
+
+function SongCard({artists, img }) {
+    function ArtistsToString() {
+        let artistsString = artists[0];
+        if (artists.length > 1) {
+            for (let i = 1; i < artists.length; i++) {
+                const artist = artists[i];
+                artistsString += `, ${artist}`;
+            }
+        }
+        return artistsString;
+    }
+
+    return (
+        <div className="song-container">
+            <div className="img-container">
+                <img src={img} />
+            </div>
+            <div className="song-info">
+                <p>{ArtistsToString()}</p>
+            </div>
+        </div>
+    );
+}
