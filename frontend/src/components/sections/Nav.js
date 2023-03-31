640af6d5a7a2fa3798b38df955c90947ca6aa7f9
agraham02 <acgraham02@gmail.com>
2023-03-31 08:00:18 -0400

    updated nav component

--- frontend/src/components/sections/Nav.js
+++ frontend/src/components/sections/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 import Footer from "./Footer";
 import "./Nav.css";
 
@@ -7,20 +7,21 @@ export default function Nav() {
     return (
         <nav>
             <div>
-                <Link to="/home">
-                    <div>Home</div>
-                </Link>
-                <Link to="/search">
-                    <div>Search</div>
-                </Link>
-                <Link to="/profile">
-                    <div>Profile</div>
-                </Link>
-                <Link to="/settings">
-                    <div>Settings</div>
-                </Link>
+                <NavLink name={"Home"} path={"/home"} />
+                <NavLink name={"Search"} path={"/search"} />
+                <NavLink name={"Profile"} path={"/profile"} />
+                <NavLink name={"Settings"} path={"/settings"} />
             </div>
             <Footer style={{ fontSize: ".8rem" }} />
         </nav>
+    );
+}
+
+function NavLink({ name, path }) {
+    const active = useMatch(path);
+    return (
+        <Link to={path} style={{textDecoration: "none"}}>
+            <div className={`nav-link ${active ? "active" : ""}`}>{name}</div>
+        </Link>
     );
 }
