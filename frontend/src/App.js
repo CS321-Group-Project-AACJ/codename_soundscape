84514a745bb9251a92da559f7919e6961e03edf4
agraham02 <acgraham02@gmail.com>
2023-03-31 08:53:04 -0400

    added settings screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,9 @@ import PageHeader from "components/sections/PageHeader";
 import { Route, Routes } from "react-router-dom";
 import DemoScreen from "screens/Demo/DemoScreen";
 import LoginScreen from "screens/Login/LoginScreen";
-// import LoginScreen from "screens/LoginScreen/LoginScreen";
 import UserProfileScreen from "screens/UserProfile/UserProfileScreen";
 import "./App.css";
+import SettingsScreen from "screens/Settings/SettingsScreen";
 
 function App() {
     return (
@@ -21,6 +21,7 @@ function App() {
             <Routes>
                 <Route path="/profile" element={<UserProfileScreen />} />
                 <Route path="demo" element={<DemoScreen />} />
+                <Route path="/settings" element={<SettingsScreen />} />
             </Routes>
         </div>
     );
---
+++ frontend/src/screens/Settings/SettingsScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./SettingsScreen.css";
+
+export default function SettingsScreen() {
+  return (
+    <div>Settings Screen</div>
+  )
+}
