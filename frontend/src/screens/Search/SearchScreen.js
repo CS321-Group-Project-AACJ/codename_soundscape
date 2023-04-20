5a9d0fc10becbaeec14e03bee3cb29d0fbd36412
JackKelz <125411033+JackKelz@users.noreply.github.com>
2023-04-19 22:11:35 -0400

    Add some functionality of search page

--- frontend/src/screens/Search/SearchScreen.js
+++ frontend/src/screens/Search/SearchScreen.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import "./SearchScreen.css";
 
+
 export default function SearchScreen() {
+
+ function handleSubmit(event) {
+    let search = document.getElementById("search").value;
+    alert(search);
+ }
   return (
-    <div>SearchScreen</div>
+    <div class = "search">
+      
+      <h1>Search</h1>
+      {/* <form> */}
+        <label htmlFor="search">Search:</label>
+        <input
+          type="text"
+          id="search"
+          name="search"
+        />
+        <button type="button" onClick={handleSubmit} class="custom-btn">Submit</button>
+        {/* </form> */}
+        
+    </div>
+    
   )
 }
+
