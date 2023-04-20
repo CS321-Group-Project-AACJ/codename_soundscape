87cd7acdb9483ba2551ab28eb70dc3f0df708662
agraham02 <acgraham02@gmail.com>
2023-04-19 22:56:01 -0400

    user card layout

--- frontend/src/screens/Home/UserCard.js
+++ frontend/src/screens/Home/UserCard.js
@@ -5,34 +5,48 @@ function UserCard({ user }) {
     return (
         <div className="user-card-container">
             <div className="user-info">
-                <div className="img-container">
-                    <img src={user.image} alt={user.name} />
+                <div className="left">
+                    <div className="img-container">
+                        <img src={user.image} alt={user.name} />
+                    </div>
+                    <div>
+                        <div style={{ margin: "0 10px", fontSize: "1.3rem" }}>
+                            username
+                        </div>
+                        <div style={{ margin: "0 10px", fontSize: ".8rem" }}>
+                            4 min. ago
+                        </div>
+                    </div>
                 </div>
-                <div style={{ margin: "0 10px", fontSize: "1.3rem" }}>
-                    username
-                </div>
+                <div >...</div>
             </div>
             <div className="song-info">
-                <div className="img-container">
-                    <img src={user.song} alt={user.name} />
+                <div className="left">
+                    <div className="img-container">
+                        <img src={user.song} alt={user.name} />
+                    </div>
+                    <div className="song-detail">
+                        {/* <div className="button-song"> */}
+                        <p
+                            className="user-card-title"
+                            style={{ margin: "0 10px", fontSize: "1.8rem" }}
+                        >
+                            {user.title}
+                        </p>
+                        <p
+                            className="user-card-artist"
+                            style={{ margin: "0 10px", fontSize: "1rem" }}
+                        >
+                            {user.artist}
+                        </p>
+                        {/* </div> */}
+                    </div>
                 </div>
-                <div className="song-detail">
-                    {/* <div className="button-song"> */}
-                    <p
-                        className="user-card-title"
-                        style={{ margin: "0 10px", fontSize: "1.8rem" }}
-                    >
-                        {user.title}
-                    </p>
-                    <p
-                        className="user-card-artist"
-                        style={{ margin: "0 10px", fontSize: "1rem" }}
-                    >
-                        {user.artist}
-                    </p>
-                    {/* </div> */}
+                <div className="interactions">
+                    <div>heart</div>
+                    <div>playlist</div>
+                    <div>queue</div>
                 </div>
-                <div>{"->"}</div>
             </div>
         </div>
     );
